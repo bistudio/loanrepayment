b2bbfcc18bc064ad5642ec6a484e05b696187586
bistudio <basswrks@hotmail.com>
2020-03-18 01:59:34 +0000

    initial file creation

--- loanrepayment.py
+++ loanrepayment.py
@@ -1,11 +1,28 @@
 import datetime as dt
 import random as rnd
+import pyodbc as pyodbc
 
 # variables
 
 try:
-    userid = str(input("Please enter userid!"))
-    login_name = str(input("Please enter login name"))
-    pwd = str(input("Please enter your password!"))
+    userid = str(input("Please enter userid: "))
+    login_name = str(input("Please enter login name: "))
+    pwd = str(input("Please enter your password: "))
 except:
     raise ValueError("You have entered incorrect login details, please check and retry!")
+
+server = 'tcp:bistudiosqlserver.database.windows.net'
+database = 'pythonsdb'
+username = 'bistudioadmin'
+password = 'Pyramid7!'
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+
+                      ';PWD='+password)
+cursor = cnxn.cursor()
+
+
+# Sample select query
+cursor.execute("SELECT @@version;")
+row = cursor.fetchone()
+while row:
+    print(row[0])
+    row = cursor.fetchone()
